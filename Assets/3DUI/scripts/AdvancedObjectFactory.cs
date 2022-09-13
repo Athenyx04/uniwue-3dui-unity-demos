948244e91dcac4c6402c75dbff67aad7383c2e9c
Manuel Calvo Martin <manuel.calvo-martin@stud-mail.uni-wuerzburg.de>
2022-09-13 13:51:50 +0200

    Advanced Object Factory improved to 4 Objects via Dropdown

--- Assets/3DUI/scripts/AdvancedObjectFactory.cs
+++ Assets/3DUI/scripts/AdvancedObjectFactory.cs
@@ -1,14 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class AdvancedObjectFactory : MonoBehaviour
 {
+    public Dropdown prefabObject;
+    public GameObject CylinderPrefab;
+    public GameObject CapsulePrefab;
     public GameObject BlockPrefab;
     public GameObject SpherePrefab;
+    private GameObject SelectedPrefab;
+    private int prefabType;
     private List<GameObject> BlocksCreated = new List<GameObject>();
     private List<GameObject> SpheresCreated = new List<GameObject>();
+    private List<GameObject> CylindersCreated = new List<GameObject>();
+    private List<GameObject> CapsulesCreated = new List<GameObject>();
+    private List<GameObject> SelectedList;
+
+    void Start()
+    {
+        SelectedPrefab = CylinderPrefab;
+        SelectedList = CylindersCreated;
+
+        prefabObject.onValueChanged.AddListener(delegate
+        {
+            prefabObjectHasChanged(prefabObject);
+        }
+        );
+    }
+
+    public void prefabObjectHasChanged(Dropdown sender)
+    {
+        prefabType = sender.value;
+        switch (sender.value)
+        {
+            case 0:
+                SelectedPrefab = CylinderPrefab;
+                SelectedList = CylindersCreated;
+                break;
+            case 1:
+                SelectedPrefab = SpherePrefab;
+                SelectedList = SpheresCreated;
+                break;
+            case 2:
+                SelectedPrefab = BlockPrefab;
+                SelectedList = BlocksCreated;
+                break;
+            case 3:
+                SelectedPrefab = CapsulePrefab;
+                SelectedList = CapsulesCreated;
+                break;
+            default:
+                Debug.LogError("Dropdown Selection Failed! Dropped back to Cylinder");
+                SelectedPrefab = CylinderPrefab;
+                SelectedList = CylindersCreated;
+                break;
+        }
+    }
+
+    public void CreatePrefab(Transform Where)
+    {
+        GameObject prefabInstance = Instantiate(SelectedPrefab, Where.position, Quaternion.identity);
+
+        prefabInstance.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        SelectedList.Add(prefabInstance);
+    }
+
+    public void DestroyAllOfType()
+    {
+        foreach (var b in SelectedList)
+        {
+            Destroy(b);
+        }
+    }
 
     public void CreateBlockPrefab(Transform Where)
     {
@@ -44,7 +110,13 @@ public class AdvancedObjectFactory : MonoBehaviour
 
     public void DestroyEveythingCreated()
     {
-        DestroyAllCreatedBlocks();
-        DestroyAllCreatedSpheres();
+        SelectedList = CylindersCreated;
+        DestroyAllOfType();
+        SelectedList = SpheresCreated;
+        DestroyAllOfType();
+        SelectedList = BlocksCreated;
+        DestroyAllOfType();
+        SelectedList = CapsulesCreated;
+        DestroyAllOfType();
     }
 }
